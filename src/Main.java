38941151a8f3837dbe192c4f2c2cf95ce8309ef1
Tusk98 <henryracoomb@gmail.com>
2019-01-02 20:25:28 -0500

    Created base code to connect to local MySQL

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     private static final String USERNAME = "dbuser";
     private static final String PASSWORD = "dbpassword";
     private static final String CONN_STRING = "jdbc:mysql://localhost/scoretracker";
 
-    public static void main (String [] args) {
+    public static void main (String [] args) throws SQLException {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            System.out.println("Connected.");
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+
 
     }
 }
